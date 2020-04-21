72753ef8ebff03e7b2560efbbb3687bfab9451b7
Trương Thuận Tài <trthtai92@gmail.com>
2020-04-21 16:46:38 +0700

    - Update art dependency.

--- src/GaugeProgress.js
+++ src/GaugeProgress.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { View, Platform, ViewPropTypes, AppState } from 'react-native';
-import { Surface, Shape, Path, Group } from '../../react-native/Libraries/ART/ReactNativeART';
+import { Surface, Shape, Path, Group } from '@react-native-community/art';
 import MetricsPath from 'art/metrics/path';
-const ActiveState = "active"
+const ActiveState = 'active';
 
 export default class GaugeProgress extends React.Component {
   constructor(props: Props) {
@@ -24,7 +24,15 @@ export default class GaugeProgress extends React.Component {
   circlePath(cx, cy, r, startDegree, endDegree) {
     let p = Path();
     p.path.push(0, cx + r, cy);
-    p.path.push(4, cx, cy, r, startDegree * Math.PI / 180, endDegree * Math.PI / 180, 1);
+    p.path.push(
+      4,
+      cx,
+      cy,
+      r,
+      (startDegree * Math.PI) / 180,
+      (endDegree * Math.PI) / 180,
+      1
+    );
     return p;
   }
 
@@ -39,36 +47,64 @@ export default class GaugeProgress extends React.Component {
   }
 
   render() {
-    const { size, width, tintColor, backgroundColor, style, stroke, strokeCap, rotation, cropDegree, children } = this.props;
-    const backgroundPath = this.circlePath(size / 2, size / 2, size / 2 - width / 2, 0, (360 * 99.9 / 100) - cropDegree);
+    const {
+      size,
+      width,
+      tintColor,
+      backgroundColor,
+      style,
+      stroke,
+      strokeCap,
+      rotation,
+      cropDegree,
+      children,
+    } = this.props;
+    const backgroundPath = this.circlePath(
+      size / 2,
+      size / 2,
+      size / 2 - width / 2,
+      0,
+      (360 * 99.9) / 100 - cropDegree
+    );
 
     const fill = this.extractFill(this.props.fill);
-    const circlePath = this.circlePath(size / 2, size / 2, size / 2 - width / 2, 0, ((360 * 99.9 / 100) - cropDegree) * fill / 100);
+    const circlePath = this.circlePath(
+      size / 2,
+      size / 2,
+      size / 2 - width / 2,
+      0,
+      (((360 * 99.9) / 100 - cropDegree) * fill) / 100
+    );
     if (!this.state.isVisible) {
       return null;
     }
     return (
       <View style={style}>
-        <Surface
-          width={size}
-          height={size}
+        <Surface width={size} height={size}>
+          <Group
+            rotation={rotation + cropDegree / 2}
+            originX={size / 2}
+            originY={size / 2}
           >
-          <Group rotation={rotation + cropDegree / 2} originX={size / 2} originY={size / 2}>
-            <Shape d={backgroundPath}
-                   strokeDash={stroke}
-                   stroke={backgroundColor}
-                   strokeWidth={width}
-                   strokeCap={strokeCap}/>
-            <Shape d={circlePath}
-                   strokeDash={stroke}
-                   stroke={tintColor}
-                   strokeWidth={width}
-                   strokeCap={strokeCap}/>
+            <Shape
+              d={backgroundPath}
+              strokeDash={stroke}
+              stroke={backgroundColor}
+              strokeWidth={width}
+              strokeCap={strokeCap}
+            />
+            <Shape
+              d={circlePath}
+              strokeDash={stroke}
+              stroke={tintColor}
+              strokeWidth={width}
+              strokeCap={strokeCap}
+            />
           </Group>
         </Surface>
         {typeof children === 'function' ? children(fill) : children}
       </View>
-    )
+    );
   }
 }
 
@@ -83,7 +119,11 @@ GaugeProgress.propTypes = {
   backgroundColor: PropTypes.string,
   rotation: PropTypes.number,
   cropDegree: PropTypes.number,
-  children: PropTypes.oneOfType([PropTypes.func, PropTypes.object, PropTypes.array])
+  children: PropTypes.oneOfType([
+    PropTypes.func,
+    PropTypes.object,
+    PropTypes.array,
+  ]),
 };
 
 GaugeProgress.defaultProps = {
